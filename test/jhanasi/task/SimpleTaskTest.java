c1924b741a2c7b55d52ac974165055ceb9008a18
Nick <nick.bell7@live.com>
2016-08-11 10:07:08 -0700

    Basic test added to the SimpleTask.java class (SimpleTaskTest.java class).

--- test/jhanasi/task/SimpleTaskTest.java
+++ test/jhanasi/task/SimpleTaskTest.java
@@ -16,48 +16,74 @@
  */
 package jhanasi.task;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Nick
  */
 public class SimpleTaskTest {
-    
+
+    private File folder;
+    private File file;
+
     public SimpleTaskTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        this.folder = new File("testFolder");
+        if (!this.folder.mkdir())
+            throw new RuntimeException("SimpleTaskTest: could not create folder");
+        this.file = new File(this.folder, "test.txt");
+        try {
+            if (!this.file.createNewFile())
+                throw new RuntimeException("SimpleTaskTest: could not create file");
+        } catch (IOException ex) {
+            Logger.getLogger(SimpleTaskTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            Files.write(this.file.toPath(), "hello world".getBytes());
+        } catch (IOException ex) {
+            Logger.getLogger(SimpleTaskTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @After
     public void tearDown() {
+        if (!this.file.delete())
+            throw new RuntimeException("SimpleTaskTest: could not delete file");
+        if (!this.folder.delete())
+            throw new RuntimeException("SimpleTaskTest: could not delete folder");
     }
 
     /**
      * Test of delegate method, of class SimpleTask.
+     * 
+     * @ToDo move some code to setup?
      */
     @Test
     public void testDelegate() {
         System.out.println("delegate");
-        SimpleTask instance = null;
+        SimpleTask instance = new SimpleTask(this.folder.toPath());
         instance.delegate();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
